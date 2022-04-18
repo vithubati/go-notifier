596374da636677809516a6a9f8e185b7a9980270
Vithu R <vithu123@hotmail.com>
2022-04-17 23:47:45 -0400

    mysql update implementation

---
+++ store/mysql/update.go
@@ -0,0 +1,52 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+)
+
+// delivered will update the delivery's status to "DELIVERED" for the provided
+// delivery id
+func delivered(ctx context.Context, db *sql.DB, id string) error {
+	const (
+		updateDelivery = `UPDATE delivery SET status = 'DELIVERED', updatedAt = CURRENT_TIMESTAMP WHERE id = ?`
+	)
+
+	rslt, err := db.ExecContext(ctx, updateDelivery, id)
+	if err != nil {
+		return err
+	}
+	r, err := rslt.RowsAffected()
+	if err != nil {
+		return errors.New(fmt.Sprintf("updating delivery state, %s", err.Error()))
+	}
+	if r <= 0 {
+		return fmt.Errorf("no rows affected when updating delivery")
+	}
+
+	return nil
+}
+
+// failed will update the delivery's status to "FAILED" for the provided
+// delivery id
+func failed(ctx context.Context, db *sql.DB, id string) error {
+	const (
+		updateDelivery = `UPDATE delivery SET status = 'FAILED', attempt = attempt + 1, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`
+	)
+
+	rslt, err := db.ExecContext(ctx, updateDelivery, id)
+	if err != nil {
+		return err
+	}
+	r, err := rslt.RowsAffected()
+	if err != nil {
+		return errors.New(fmt.Sprintf("updating delivery state, %s", err.Error()))
+	}
+	if r <= 0 {
+		return fmt.Errorf("no rows affected when updating delivery")
+	}
+
+	return nil
+}
